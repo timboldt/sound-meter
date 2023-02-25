806d0e31eba122f6bc4d676a247c3566e96cdf2b
timboldt <tim.boldt@gmail.com>
2023-02-25 12:32:07 -0800

    Reformatting

--- sound-meter.ino
+++ sound-meter.ino
@@ -1,17 +1,15 @@
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
 
 Adafruit_NeoPixel neo_featherwing(32, D8, NEO_GRB + NEO_KHZ800);
 
-void setup()
-{
+void setup() {
     Serial.begin(1200);
     pinMode(LED_BUILTIN, OUTPUT);
     neo_featherwing.begin();
 }
 
-void loop()
-{
+void loop() {
     const float alpha = 0.9;
     const float beta = 1.0 - alpha;
     static float val[4] = {0, 0, 0, 0};
@@ -19,19 +17,15 @@ void loop()
     val[1] = alpha * val[1] + beta * val[0];
     val[2] = alpha * val[2] + beta * val[1];
     val[3] = alpha * val[3] + beta * val[2];
-    for (int row = 0; row < 4; row++)
-    {
-        for (int col = 0; col < 8; col++)
-        {
-            if (val[row] < col)
-            {
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 8; col++) {
+            if (val[row] < col) {
                 neo_featherwing.setPixelColor(row * 8 + col, 0);
-            }
-            else
-            {
+            } else {
                 const uint16_t range = 30000;
                 const uint16_t offset = 25000;
-                const uint16_t hue = offset - (uint16_t)(min(val[row], col + 1) * range / 8);
+                const uint16_t hue =
+                    offset - (uint16_t)(min(val[row], col + 1) * range / 8);
                 const uint32_t hsv = neo_featherwing.ColorHSV(hue, 255, 4);
                 neo_featherwing.setPixelColor(row * 8 + col, hsv);
             }
