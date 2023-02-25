7f9a28b6eddc12f150ac573b2586c27b88de0536
timboldt <tim.boldt@gmail.com>
2023-02-25 15:52:08 -0800

    Simple sound detection working

--- sound-meter.ino
+++ sound-meter.ino
@@ -1,34 +1,58 @@
 #include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
 
-Adafruit_NeoPixel neo_featherwing(32, D8, NEO_GRB + NEO_KHZ800);
+// const int16_t I2S_BCLK = 10;
+// const int16_t I2S_LRCLK = I2S_BCLK + 1;  // Mandatory: BCLK + 1.
+// const int16_t I2S_DOUT = 12;
+
+// I2S i2s(INPUT);
+
+const int16_t MIC_PIN = 10;
+
+const int16_t NEO_FEATHERWING_PIN = 8;
+Adafruit_NeoPixel neo_featherwing(32, NEO_FEATHERWING_PIN,
+                                  NEO_GRB + NEO_KHZ800);
 
 void setup() {
-    Serial.begin(1200);
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+
     neo_featherwing.begin();
+
+    pinMode(MIC_PIN, INPUT);
+
+    // i2s.setDATA(I2S_DOUT);
+    // i2s.setBCLK(I2S_BCLK);
+    // i2s.setBitsPerSample(16);
+    // i2s.setFrequency(22050);
+    // i2s.begin();
 }
 
 void loop() {
+    static uint16_t on_cnt = 0;
+    if (digitalRead(MIC_PIN)) {
+        on_cnt = min(on_cnt + 1, 8);
+    } else {
+        on_cnt = max(on_cnt - 1, 0);
+    }
+    Serial.println(on_cnt);
+
     const float alpha = 0.9;
     const float beta = 1.0 - alpha;
     static float val[4] = {0, 0, 0, 0};
-    val[0] = min(8, max(0, random(3) + val[0] - 1));
+    val[0] = min(8, max(0, on_cnt));
     val[1] = alpha * val[1] + beta * val[0];
     val[2] = alpha * val[2] + beta * val[1];
     val[3] = alpha * val[3] + beta * val[2];
     for (int row = 0; row < 4; row++) {
         for (int col = 0; col < 8; col++) {
-            if (val[row] < col) {
-                neo_featherwing.setPixelColor(row * 8 + col, 0);
-            } else {
-                const uint16_t range = 30000;
-                const uint16_t offset = 25000;
-                const uint16_t hue =
-                    offset - (uint16_t)(min(val[row], col + 1) * range / 8);
-                const uint32_t hsv = neo_featherwing.ColorHSV(hue, 255, 4);
-                neo_featherwing.setPixelColor(row * 8 + col, hsv);
-            }
+            const uint16_t range = 31000;
+            const uint16_t offset = 31000;
+            const uint16_t hue =
+                offset - (uint16_t)(min(val[row], col + 1) * range / 8);
+            const uint8_t brightness = 64 * max(0.0, min(1.0, val[row] - col));
+            const uint32_t hsv = neo_featherwing.gamma32(
+                neo_featherwing.ColorHSV(hue, 255, brightness));
+            neo_featherwing.setPixelColor(row * 8 + col, hsv);
         }
     }
     neo_featherwing.show();
